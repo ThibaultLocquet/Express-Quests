a10c3cc07181f62240fd93ec4f41925e356991dd
ThibaultLocquet <thibaultlocquetweb@gmail.com>
2022-12-01 13:54:26 +0100

    express 5

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.get("/api/users/:id", movieHandlers.getUserById);
 app.post("/api/users", movieHandlers.postUsers);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", movieHandlers.updateUsers);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", movieHandlers.deleteUsers);
 
 app.listen(port, (err) => {
   if (err) {
--- movieHandlers.js
+++ movieHandlers.js
@@ -96,6 +96,42 @@ const updateMovie = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
+
+const deleteUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the users");
+    });
+};
+
 const getUsers = (req, res) => {
   database
     .query("select * from users")
@@ -174,4 +210,6 @@ module.exports = {
   postUsers,
   updateMovie,
   updateUsers,
+  deleteMovie,
+  deleteUsers,
 };
